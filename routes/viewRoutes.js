5c68ff8d370510e50bb90be0876eb0f59fce7e3a
tosin-webdev <oladejit5@gmail.com>
2023-02-28 06:26:57 +0100

    updates

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const viewsController = require('../controllers/viewController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
 router.get('/signup', viewsController.signup);
 router.get('/signin', viewsController.signin);
-router.get('/tour/:slug', viewsController.getTour);
+router.get('/tour/:slug', authController.protect, viewsController.getTour);
 
 module.exports = router;
 // api/v1/tours/5c88fa8cf4afda39709c2970
