2c913f69177aea1f83a976f0e2b0f006e1c98e86
tosin-webdev <oladejit5@gmail.com>
2023-03-06 03:31:24 +0100

    updates

--- utils/email.js
+++ utils/email.js
@@ -1,17 +1,63 @@
 const nodemailer = require('nodemailer');
+const ejs = require('ejs');
 const catchAsync = require('./catchAsync');
+
+new Email(user, url).sendWelcome();
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `oladeji Tosin <${process.env.EMAIL_FROM}>`;
+  }
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  send(template, subject) {
+    // send the actual email
+    //1) Render HTML based on ejs template
+    const html = ejs.renderFile(`${__dirname}/../views/emails/${template}.pug`);
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: options.message,
+    };
+  }
+
+  sendWelcome() {
+    this.send('Welcome', 'Welcome to elimpco');
+  }
+};
 
 const sendEmail = catchAsync(async (options) => {
   // 1) Create a transporter(an object that is able to send mail)
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+  // const transporter = nodemailer.createTransport({
+  //   host: process.env.EMAIL_HOST,
+  //   port: process.env.EMAIL_PORT,
 
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  //   auth: {
+  //     user: process.env.EMAIL_USERNAME,
+  //     pass: process.env.EMAIL_PASSWORD,
+  //   },
+  // });
 
   //   2) Define the email options
   const mailOptions = {
@@ -26,4 +72,4 @@ const sendEmail = catchAsync(async (options) => {
   await transporter.sendMail(mailOptions);
 });
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
