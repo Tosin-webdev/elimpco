b64879af4e570f99547cc2f26b88c15d05892452
tosin-webdev <oladejit5@gmail.com>
2023-04-17 17:37:28 +0100

    updates

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const AppError = require('./utils/appError');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
-const bookingRouter = require('./routes/bookingRoutes');
+// const bookingRouter = require('./routes/bookingRoutes');
 
 const helmet = require('helmet');
 
@@ -95,7 +95,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
-app.use('/api/v1/bookings', bookingRouter);
+// app.use('/api/v1/bookings', bookingRouter);
 
 // app.all('*', (req, res, next) => {
 //   // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
