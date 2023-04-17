261282a05bc993908d1c106f0cb6dd36e5ab3190
tosin-webdev <oladejit5@gmail.com>
2023-04-17 21:35:16 +0100

    updates

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,21 +1,21 @@
-// const express = require('express');
-// const bookingController = require('../controllers/bookingController');
-// const authController = require('../controllers/authController');
+const express = require('express');
+const bookingController = require('../controllers/bookingController');
+const authController = require('../controllers/authController');
 
-// // const router = express.Router({ mergeParams: true });
-// const router = express.Router();
+// const router = express.Router({ mergeParams: true });
+const router = express.Router();
 
-// // console.log(router.route(''));
-// router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession);
+// console.log(router.route(''));
+router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession);
 
-// router.use(authController.restrictTo('admin', 'lead-guide'));
+router.use(authController.restrictTo('admin', 'lead-guide'));
 
-// router.route('/').get(bookingController.getAllBookings).post(bookingController.createBooking);
+router.route('/').get(bookingController.getAllBookings).post(bookingController.createBooking);
 
-// router
-//   .route('/:id')
-//   .get(bookingController.getBooking)
-//   .patch(bookingController.updateBooking)
-//   .delete(bookingController.deleteBooking);
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
-// module.exports = router;
+module.exports = router;
