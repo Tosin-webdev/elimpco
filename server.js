4c3a16dbdc4e611b2ed46d816d35042f0eba1d80
tosin-webdev <oladejit5@gmail.com>
2023-04-26 16:27:27 +0100

    updates

--- server.js
+++ server.js
@@ -16,26 +16,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
-// const connectDB = async () => {
-//   try {
-//     // mongodb connection string
-//     const con = await mongoose.connect(process.env.MONGO_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log(`mongoDB connected: ${con.connection.host}`);
-//   } catch (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
-// };
-// connectDB();
-
-// process.on('unhandledRejection', (err) => {
-//   console.log(err);
-//   console.log('UNHANDLED REJECTION! shuttiing down...');
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
